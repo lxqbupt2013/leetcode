4b1e3d9bd6bcf1fe0a8974cf538a7ed9a5acf215
lxq <liu_xiaqi@foxmail.com>
2019-01-29 12:13:53 +0800

    'findtarget'

--- findTarget.js
+++ findTarget.js
@@ -7,35 +7,28 @@ var findTarget = function(matrix, target) {
     var tempArr;
 
     for (var m = 0; m < matrix.length; m ++) {
-        if (tempArr === undefined) {
-            tempArr = matrix[m];
-        } else {
-            var tempArr2 = tempArr;
-            tempArr = tempArr2[m];
-            console.log(tempArr)
+        tempArr = matrix[m];
 
-            var rowLen = tempArr.length;
-            var colLen = tempArr[0].length;
+        var i = tempArr.length - 1;
+        var colLen = tempArr[0].length
+        var j = 0;
 
-            for (var k = 0; k < tempArr2.length; k++) {
-                tempArr = tempArr2[k];
-                if(typeof tempArr[0][0] === 'number') {
-                    console.log('jinlaile1')
-                    var i=0;
-                    var j = tempArr[0].length-1;
-                    while (i >=0 && j < colLen ) {
-                        console.log('jinlaile2')
-                        if(tempArr[i][j] === target) {
-                            return true;
-                        } else if(tempArr[i][j] > target) {
-                            i -= 1;
-                        } else {
-                            j += 1;  //matrix[i][j]) < target
-                        }
-                    }                    
-                } else {
-                    tempArr = tempArr[i];
-                }
+        // 如果该面最大的数比target还小，说明在下一个面中，直接开始下一次迭代
+        if(matrix[m][0][0] === target) {
+            return true;
+        } else if(matrix[m][tempArr.length-1][colLen-1] < target) {
+            continue;
+        }
+
+        // 二维数组的比较，从左下角开始
+        while (i >= 0 && j < colLen ) {
+
+            if(tempArr[i][j] === target) {
+                return true;
+            } else if(tempArr[i][j] > target) {
+                i -= 1;
+            } else {
+                j += 1;  //matrix[i][j]) < target
             }
         }
     }
@@ -43,8 +36,6 @@ var findTarget = function(matrix, target) {
     return false;
 }
 
-
 var matrix = [[[1,2,3,4],[2,3,4,5],[5,6,7,8]],[[9,10,11,12],[13,14,15,16],[17,18,19,20]]]
-
 
 findTarget(matrix,10)
